b4626529d2295b40d12e250ce62844e0b9744064
delusion <viii98859616@gmail.com>
2016-10-20 22:18:42 +0800

    add LearnGit Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LearnGit Test
                 </div>
 
                 <div class="links">
